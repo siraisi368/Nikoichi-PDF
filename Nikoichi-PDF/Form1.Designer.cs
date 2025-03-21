9d8e243796872b59d95bca1402fafa94615ae8e9
Masato Siraisi <56631381+siraisi368@users.noreply.github.com>
2025-03-22 01:35:23 +0900

    [add] アイコン

--- Nikoichi-PDF/Form1.Designer.cs
+++ Nikoichi-PDF/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
@@ -189,6 +190,7 @@
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Nikoichi-PDF";
             this.Load += new System.EventHandler(this.Form1_Load);
